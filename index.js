45846c97e88423cfe5eed24fec930e46c8e675cf
Shafanamar <149698834+Shafanamar@users.noreply.github.com>
2024-10-20 15:46:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.GuildVoiceStates,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+// Load config
+const config = require('./config.json');
+client.commands = new Collection();
+
+// Load command files
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.data.name, command);
+}
+
+// Load event files
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+    const event = require(`./events/${file}`);
+    const eventName = file.split('.')[0];
+    client.on(eventName, event.bind(null, client));
+}
+
+client.login(config.token);
